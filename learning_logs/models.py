611ec8060d41319b71f287d260a03e17f67bdd07
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-22 18:05:29 +0300

    Added Entry model.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,3 +12,17 @@ class Topic(models.Model):
     def __unicode__(self):
         """Return a string representation of the model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __unicode__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
