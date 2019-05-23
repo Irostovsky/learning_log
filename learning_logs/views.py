245b70409b5cd8e3f9f3d7658d79cb8d48200ebc
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-23 17:58:35 +0300

    Add new topic form.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -23,3 +27,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Create new topic"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
