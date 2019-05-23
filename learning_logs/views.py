dcb7c83578fa1904b90516a7f6feeb38ea97d7f4
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-23 10:41:11 +0300

    Added index page.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
