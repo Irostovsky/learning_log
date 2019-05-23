127ba87f0d9549be7cd789839524052cb909e69d
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-23 16:50:40 +0300

    Show topics.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,8 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show topic with entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
