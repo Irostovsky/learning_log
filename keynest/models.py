9f8b7e4b8ea71720e07a330f849f3cf5f73bdab8
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-28 17:42:56 +0300

    Load keenest key in hack way.

--- keynest/models.py
+++ keynest/models.py
@@ -8,10 +8,10 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Key(models.Model):
     """A topic the user is learning about"""
-    keynest_id = models.IntegerField()
+    keynest_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model."""
-        return str(self.keynest_id) + self.name
+        return str(self.keynest_id) + ' ' + self.name
--- keynest/views.py
+++ keynest/views.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 from django.views import generic, View
+from django.shortcuts import redirect, reverse
 
 from .models import Key
+from .services import load_keys
 
 
 class IndexView(generic.ListView):
+    model = Key
     template_name = 'keynest/index.html'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Key.objects.order_by('name')
+        return Key.objects.all().order_by('name')
 
 
 class LoadKeysView(View):
     def get(self, request, *args, **kwargs):
-        context = {'message': 'Keys are loaded'}
-        return render(request, 'keynest/index.html', context)
+        load_keys()
+        return redirect(reverse('keynest:index'))
 
 
