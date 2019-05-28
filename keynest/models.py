258b762506c28e31b4b9b4ffb136e0cddeedb296
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-28 15:51:23 +0300

    Show all keys.

--- keynest/models.py
+++ keynest/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Key(models.Model):
+    """A topic the user is learning about"""
+    keynest_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return str(self.keynest_id) + self.name
--- keynest/views.py
+++ keynest/views.py
@@ -8,11 +8,13 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
+from .models import Key
+
 
 class IndexView(generic.ListView):
     template_name = 'keynest/index.html'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return []
+        return Key.objects.order_by('name')
 
