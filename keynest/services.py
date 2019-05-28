c7aaab1b60def7ea0a7af3a8b5bf20aaa9169790
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-05-28 18:38:27 +0300

    Use udpate_or_create.

--- keynest/services.py
+++ keynest/services.py
@@ -1,11 +1,20 @@
 
 from .models import Key
-from .api import key_list
+from .api import key_list#, stores
 
 
 def load_keys(token):
     for item in key_list(token):
-        if not Key.objects.filter(keynest_id=item['KeyId']):
-            key = Key(keynest_id=item['KeyId'], name=item['KeyName'])
-            key.save()
+        Key.objects.update_or_create(
+            keynest_id=item['KeyId'],
+            defaults={
+                'name': item['KeyName']
+            })
+
+def load_stores(token):
+    pass
+    # for item in stores(token):
+    #     if not Key.objects.filter(keynest_id=item['KeyId']):
+    #         key = Key(keynest_id=item['KeyId'], name=item['KeyName'])
+    #         key.save()
 
